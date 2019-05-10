3d2e73df928793ebebeec22f8a41d5cd742f687c
Agnieszka Matyja <agmatyja@gmail.com>
2019-05-10 21:57:04 +0200

    added next films and photo

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-var element = React.createElement('div', {}, 'Hello world!');
-ReactDOM.render(element, document.getElementById('app'));
+
+ var movies = [
+  {
+    id: 1,
+    title: 'Harry Potter',
+    desc: 'film o czarodzieju',
+	photo:''
+  },
+  {
+    id: 2,
+    title: 'Król Lew',
+    desc: 'Film o królu sawanny',
+	photo:''
+  },
+  {
+    id: 3,
+    title: 'Dzień świstaka',
+    desc: 'Film wcale nie o świstaku',
+	photo:''
+  },
+  {
+    id: 4,
+    title: 'Tytanic',
+    desc: 'Film o pechowym statku',
+	photo:''
+  },
+  {
+    id: 5,
+    title: 'Chicago',
+    desc: 'Film o strasznych babach',
+	photo:'pobrane.jpg'
+  },
+    
+ ];
+
+var moviesElements = movies.map(function(movie) {
+  return React.createElement('li', {key: movie.id},
+      React.createElement('h2', {}, movie.title),
+      React.createElement('p', {}, movie.desc),
+	  React.createElement('img', {'src': movie.photo})
+    );
+});
+
 
 var element =
   React.createElement('div', {},
     React.createElement('h1', {}, 'Lista filmów'),
-    React.createElement('ul', {},
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'Harry Potter'),
-        React.createElement('p', {}, 'Film o czarodzieju')
-      ),
-      React.createElement('li', {},
-        React.createElement('h2', {}, 'Król Lew'),
-        React.createElement('p', {}, 'Film opowiadający historię króla sawanny')
-      )
-    )
-  );
+    React.createElement('ul', {}, moviesElements)
+	);
+ 
+  
+ ReactDOM.render(element, document.getElementById('app'));
